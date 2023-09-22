d9d19d867e2302a2a1fdfc25f59eaffc412400cf
Jeph <ericjeffrey.sch@gmail.com>
2023-09-22 11:22:11 +0800

    Init

---
+++ src/main/java/com/autoprov/autoprov/AutoprovisioningApplication.java
@@ -0,0 +1,13 @@
+package com.autoprov.autoprov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoprovisioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoprovisioningApplication.class, args);
+	}
+
+}
