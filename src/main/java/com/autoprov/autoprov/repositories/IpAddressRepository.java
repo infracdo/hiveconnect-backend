511a76e91550118f6662e40b5f8cc8ce21a021a3
Jeph <ericjeffrey.sch@gmail.com>
2023-09-28 11:52:28 +0800

    Fix database naming on @Query

--- src/main/java/com/autoprov/autoprov/repositories/IpAddressRepository.java
+++ src/main/java/com/autoprov/autoprov/repositories/IpAddressRepository.java
@@ -8,9 +8,9 @@ import com.autoprov.autoprov.domain.IpAddress;
 
 public interface IpAddressRepository extends CrudRepository<IpAddress, Long> {
 
-    @Query(value = "SELECT * from IpAddresses where status = \"Available\"", nativeQuery = true)
+    @Query(value = "SELECT * from ipaddresses where status = \"Available\"", nativeQuery = true)
     List<IpAddress> findAllAvailableIp();
 
-    @Query(value = "SELECT * from IpAddresses where ip_address LIKE ?1%", nativeQuery = true)
+    @Query(value = "SELECT * from ipaddresses where ip_address LIKE ?1%", nativeQuery = true)
     List<IpAddress> findAllUnderNetworkAddress(String networkAddress);
 }
