d82a74a87f8a63c4b4ee1a4dd10a09cda1939405
Jeph <ericjeffrey.sch@gmail.com>
2023-10-16 16:09:43 +0800

    Fix OLT Repo and Controllers

--- src/main/java/com/autoprov/autoprov/repositories/OltRepository.java
+++ src/main/java/com/autoprov/autoprov/repositories/OltRepository.java
@@ -5,8 +5,9 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import com.autoprov.autoprov.domain.Olt;
+import java.util.List;
 
 public interface OltRepository extends CrudRepository<Olt, Long> {
 
-    Optional<Olt> findByolt_site(String olt_site);
+    Optional<Olt> findByOlt_site(String olt_site);
 }
