3753d30da02ad3f2777668e03c03b7c71707f5e8
Jeph <ericjeffrey.sch@gmail.com>
2023-10-16 12:39:25 +0800

    Add PackageType table

---
+++ src/main/java/com/autoprov/autoprov/domain/PackageType.java
@@ -0,0 +1,35 @@
+package com.autoprov.autoprov.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity // labels an entity that can be used for JPA
+@Table(name = "networkaddresses")
+
+public class PackageType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "package_type_id", unique = true)
+    private String olt_site;
+
+    @Column(name = "upstream")
+    private String upstream;
+
+    @Column(name = "downstream")
+    private String downstream;
+}
