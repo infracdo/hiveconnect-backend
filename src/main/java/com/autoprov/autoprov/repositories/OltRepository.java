37be1159e30aebc8c17c288403ea060e7e680536
Jeph <ericjeffrey.sch@gmail.com>
2023-10-16 16:14:46 +0800

    Fix PackageType and OLT Repo and Controllers

--- src/main/java/com/autoprov/autoprov/repositories/OltRepository.java
+++ src/main/java/com/autoprov/autoprov/repositories/OltRepository.java
@@ -2,11 +2,13 @@ package com.autoprov.autoprov.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.autoprov.autoprov.domain.Olt;
 
 public interface OltRepository extends CrudRepository<Olt, Long> {
 
-    Optional<Olt> findByolt_site(String olt_site);
+    @Query(value = "SELECT * from package_types where olt_site=?1", nativeQuery = true)
+    Optional<Olt> findByOlt_site(String olt_site);
 }
