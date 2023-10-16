a9d90a274e7100b59183d4abc21379530bf46ae2
Jeph <ericjeffrey.sch@gmail.com>
2023-10-16 15:19:05 +0800

    Fix PackageTypeController

--- src/main/java/com/autoprov/autoprov/repositories/PackageRepository.java
+++ src/main/java/com/autoprov/autoprov/repositories/PackageRepository.java
@@ -2,11 +2,13 @@ package com.autoprov.autoprov.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.autoprov.autoprov.domain.PackageType;
 
 public interface PackageRepository extends CrudRepository<PackageType, Long> {
 
+    @Query(value = "SELECT * from packagetypes where package_type_id=?1", nativeQuery = true)
     Optional<Package> findBypackageTypeId(String packageTypeId);
 }
