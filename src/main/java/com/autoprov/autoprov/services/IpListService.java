76c2bd77b403189f60e32fbcf3d43b61941d4857
Jeph <ericjeffrey.sch@gmail.com>
2023-10-18 16:59:34 +0800

    Add CIDR Support

--- src/main/java/com/autoprov/autoprov/services/IpListService.java
+++ src/main/java/com/autoprov/autoprov/services/IpListService.java
@@ -39,8 +39,10 @@ public class IpListService {
             hostRange = 255;
         else if (maskBits == 29)
             hostRange = 8;
-        else
+        else if (maskBits != 24 || maskBits != 29)
             return "CIDR not supported. Only supports /24 and /29";
+        else
+            return null;
 
         Integer gatewayHost = Integer.parseInt(internetGateway);
         System.out.println("Gateway: " + gatewayHost);
