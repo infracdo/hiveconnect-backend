67a626288034919e3197d48120a7fc430e891887
Jeph <ericjeffrey.sch@gmail.com>
2023-11-09 11:22:34 +0800

    Update

--- src/main/java/com/autoprov/autoprov/controllers/AcsController.java
+++ src/main/java/com/autoprov/autoprov/controllers/AcsController.java
@@ -9,10 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin(origins = "*")
+@RestController
 public class AcsController {
 
     // -------------------Exposed APIs for Connect-Disconnect
