1d85553e940149538edd07051a00493ff8ffeac4
Jeph <ericjeffrey.sch@gmail.com>
2023-11-12 10:59:08 +0800

    Fix Olt Repo

--- src/main/java/com/autoprov/autoprov/repositories/inetRepositories/OltRepository.java
+++ src/main/java/com/autoprov/autoprov/repositories/inetRepositories/OltRepository.java
@@ -9,9 +9,9 @@ import com.autoprov.autoprov.entity.inetDomain.Olt;
 
 public interface OltRepository extends CrudRepository<Olt, Long> {
 
-    @Query(value = "SELECT * from olt where olt_site=?1", nativeQuery = true)
+    @Query(value = "SELECT * from olt where name=?1", nativeQuery = true)
     Optional<Olt> findByOlt_site(String olt_site);
 
-    @Query(value = "SELECT * from olt where olt_ip=?1", nativeQuery = true)
+    @Query(value = "SELECT * from olt where ip=?1", nativeQuery = true)
     Optional<Olt> findByOlt_ip(String olt_ip);
 }
