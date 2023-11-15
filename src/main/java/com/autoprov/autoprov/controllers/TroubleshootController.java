ce4683ae5e7675721538cc43d567e56b47ce63d0
Jeph <ericjeffrey.sch@gmail.com>
2023-11-15 17:31:25 +0800

    Update Troubleshoot Controller

--- src/main/java/com/autoprov/autoprov/controllers/TroubleshootController.java
+++ src/main/java/com/autoprov/autoprov/controllers/TroubleshootController.java
@@ -15,8 +15,8 @@ public class TroubleshootController {
     @Async("asyncExecutor")
     @GetMapping("/getStatus/{device}")
     public String getOnuStatus(@PathVariable("device") String device) {
-        String prometheusUrl = "https://dctech-prometheus.apolloglobal.net/api/v1/query?query=lo_status{job=%22ip_address%22,site_tenant=%22DCTECH%22,device_name="
-                + device + "}";
+        String prometheusUrl = "https://dctech-prometheus.apolloglobal.net/api/v1/query?query=lo_status{job=%22ip_address%22,site_tenant=%22DCTECH%22,device_name=\""
+                + device + "\"}";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
